7d07bfc55a54429ba909a25f4f32bd5bc3744f30
alihaiderdev <ah1675179@gmail.com>
2021-10-22 10:35:32 +0500

    remove some irrelavent stuff from excel.js file.

--- excel.js
+++ excel.js
@@ -5,14 +5,11 @@ document.getElementById('input').addEventListener('change', (event) => {
 });
 
 document.getElementById('button').addEventListener('click', () => {
-  //   XLSX.utils.json_to_sheet(data, 'out.xlsx');
   if (selectedFile) {
     let fileReader = new FileReader();
     fileReader.readAsBinaryString(selectedFile);
     fileReader.onload = (event) => {
-      let data = event.target.result;
-      let workbook = XLSX.read(data, { type: 'binary' });
-      console.log(workbook);
+      let workbook = XLSX.read(event.target.result, { type: 'binary' });
       workbook.SheetNames.forEach((sheet) => {
         let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet]);
         console.log(rowObject);
